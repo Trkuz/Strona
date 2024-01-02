e5c40a734b2ddf3f6cd2bb253211b94df7f8a3e3
Trkuz <108626181+Trkuz@users.noreply.github.com>
2024-01-02 17:12:13 +0100

    Update stylep.php

--- stylep.php
+++ stylep.php
@@ -20,8 +20,8 @@
              <?php if (isset($user)): ?>
                 
                 <li ><a href="stylep.php">Style Piwne</a></li>
-                <li><a href="#">O Piwie</a></li>
-                <li ><a href="#">Sensoryka</a></li>
+                <li><a href="opiwie.php">O Piwie</a></li>
+                <li ><a href="stylep.php">Sensoryka</a></li>
                 <li><a href="wyloguj.php" target="_self" class="logowanie"><?= htmlspecialchars($user["name"])?></a></li>
 
             <?php else: ?>
@@ -37,36 +37,35 @@
         
     <div class="style">
         <ul>
-            <li>Altbier</li>
-            <li>Belgian blond ale</li>
-            <li>Belgian dubbel</li>
-            <li>Berliner weisse</li>
-            <li>Bitter</li>
-            <li>Britsh golden ale</li>
-            <li>Festbie</li>
-            <li>Gose</li>
-            <li>Grodziskie</li>
-            <li>India Pale Ale</li>
-            <li>Irish Red Ale</li>
-            <li>Jasny Lager</li>
-            <li>Kellerbier</li>
-            <li>Doppelbock</li>
-            <li>Lager wiedeński</li>
-            <li>Leichbier</li>
-            <li>Marcowe</li>
-            <li>Milk stout</li>
-            <li>Braggot</li>
-            <li>Porter Bałtycki</li>
-            <li>Sahti</li>
-            <li>Saison</li>
-            <li>Rauchmaerzen</li>
-            <li>Russian Imperial Stout</li>
-            <li>Wee heavy</li>
-            <li>Weissbier</li>
-            <li>Wheat IPA</li>
-            <li>Witbier</li>
-            <li>California Common</li>
-            <li>Old Ale</li>
+            <li><a href="style/Altbier.php">Altbier</a></li>
+            <li><a href="style/BBA.php">Belgian blond ale</a></li>
+            <li><a href="style/BD.php">Belgian dubbel</a></li>
+            <li><a href="style/BerlinerWeisse.php">Berliner weisse</a></li>
+            <li><a href="style/Bitter.php">Bitter</a></li>
+            <li><a href="style/BGA.php">Britsh golden ale</a></li>
+            <li><a href="style/Gose.php">Gose</a></li>
+            <li><a href="style/Grodziskie.php">Grodziskie</a></li>
+            <li><a href="style/IPA.php">India Pale Ale</a></li>
+            <li><a href="style/IRA.php">Irish Red Ale</a></li>
+            <li><a href="style/Lager.php">Jasny Lager</a></li>
+            <li><a href="style/Kellerbier.php">Kellerbier</a></li>
+            <li><a href="style/Doppelbock.php">Doppelbock</a></li>
+            <li><a href="style/ViennaLager.php">Lager wiedeński</a></li>
+            <li><a href="style/LeichBier.php">Leichbier</a></li>
+            <li><a href="style/Marcowe.php">Marcowe</a></li>
+            <li><a href="style/MilkStout.php">Milk stout</a></li>
+            <li><a href="style/Braggot.php">Braggot</a></li>
+            <li><a href="style/Porter.php">Porter Bałtycki</a></li>
+            <li><a href="style/Sahti.php">Sahti</a></li>
+            <li><a href="style/Saison.php">Saison</a></li>
+            <li><a href="style/Rauchmarzen.php">Rauchmarzen</a></li>
+            <li><a href="style/RIS.php">Russian Imperial Stout</a></li>
+            <li><a href="style/WeeHeavy.php">Wee heavy</a></li>
+            <li><a href="style/Weissbier.php">Weissbier</a></li>
+            <li><a href="style/WhileIPA.php">Wheat IPA</a></li>
+            <li><a href="style/Witbier.php">Witbier</a></li>
+            <li><a href="style/CC.php">California Common</a></li>
+            <li><a href="style/OldAle.php">Old Ale</a></li>
 
         </ul>
     </div>
